15614deb4dc15ae32e800e0a8877a2e4efc91668
Rifat79 <33195501+Rifat79@users.noreply.github.com>
2025-08-11 19:47:24 +0600

    fix(excel export): renamed file for export of wellbe, xommcric, xoomsports, starzgames

--- src/app/pages/reports/Xoomcric/table/toolber/excelExport.tsx
+++ src/app/pages/reports/Xoomcric/table/toolber/excelExport.tsx
@@ -75,7 +75,7 @@ const ExcelExport = () => {
 
   // console.log(data)
   if (data.length > 0 && !loading)
-    return <CSVDownload target='_self' filename={'AIHistory.csv'} data={data} />
+    return <CSVDownload target='_self' filename={'XoomCric.csv'} data={data} />
 
   return (
     <div className='menu-item px-3'>
